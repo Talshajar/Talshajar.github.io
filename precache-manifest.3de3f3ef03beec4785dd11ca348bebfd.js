2d5131093a5b30292a4fb8bdd8ba67e7319fcf17
Talshajar <rocio.chavoya@gmail.com>
2019-06-20 15:21:58 -0500

    Updates

---
+++ precache-manifest.3de3f3ef03beec4785dd11ca348bebfd.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67f9001ca766b81b635c909ef9d2c9b9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dbf51e91f248d797292d",
+    "url": "/static/css/main.3d017596.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "dbf51e91f248d797292d",
+    "url": "/static/js/main.6fdc3f3c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "1e9c3f448eca582f83c14704100ab57d",
+    "url": "/static/media/devicons.1e9c3f44.svg"
+  },
+  {
+    "revision": "36c5ab9318386dc0499a0d89691e6331",
+    "url": "/static/media/devicons.36c5ab93.eot"
+  },
+  {
+    "revision": "5ed4d6468a5a0f3cf9c82f854936feab",
+    "url": "/static/media/devicons.5ed4d646.ttf"
+  },
+  {
+    "revision": "a8639dec5833a8375d22fe425b01f4eb",
+    "url": "/static/media/devicons.a8639dec.woff"
+  },
+  {
+    "revision": "74f7cdffc9b2c3f62771c7acac3f512d",
+    "url": "/static/media/grey.74f7cdff.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.054774adbe886ee6e3c29227ef1745b5.js"
+  "/precache-manifest.3de3f3ef03beec4785dd11ca348bebfd.js"
 );
 
 self.addEventListener('message', (event) => {
