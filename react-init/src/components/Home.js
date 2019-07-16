564a21aa475aded999265220592e70f7b2aefb0e
Talshajar <rocio.chavoya@gmail.com>
2019-07-16 13:48:15 -0500

    add navigation component, npm react router domm and mdboostrap install

--- react-init/src/components/Home.js
+++ react-init/src/components/Home.js
@@ -1,16 +1,19 @@
 import React, { Component } from 'react';
-// import superb from 'superb'
+import superb from 'superb'
 import weekday from '../utils/weekday';
-
+import words from '../utils/words.json';
 
 class Home extends Component {
 
+ 
+
 
     render() {
+
         return (
             <div>
                <h1>Rocio Chavoya</h1>
-               <p ><strong> {weekday()}</strong></p>
+               <p><strong>Have a nice <span ref="word">{superb.words}</span> {weekday()}</strong></p>
             </div>
         )  
     }
