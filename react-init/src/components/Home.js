db9d673b86ddeb1e165fce74653dddee54666074
Talshajar <rocio.chavoya@gmail.com>
2019-07-16 07:47:39 -0500

    fixed bugs and add the day of week with a Date()

--- react-init/src/components/Home.js
+++ react-init/src/components/Home.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
+// import superb from 'superb'
+import weekday from '../utils/weekday';
 
 
 class Home extends Component {
+
+
     render() {
         return (
             <div>
                <h1>Rocio Chavoya</h1>
-               <p>Developer Front End</p>
-               <p>UX Designer</p>
-               <p>Visual Merchandiser</p>
+               <p ><strong> {weekday()}</strong></p>
             </div>
         )  
     }
